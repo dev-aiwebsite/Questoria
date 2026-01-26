818db81503c5b9c0db9e9393e84d474ba68b6dac
aidev <dev@aiwebsiteservices.com>
2026-01-27 03:14:50 +0800

    update proxy.ts to fix  onboarding redirect logic

--- src/proxy.ts
+++ src/proxy.ts
@@ -6,37 +6,44 @@ export async function proxy(req: NextRequest) {
   const token = await getAuthToken(req);
   const isTestMode = process.env.TEST_MODE === "true";
 
-  if(!isTestMode){
+  if (!isTestMode) {
 
     const pathname = req.nextUrl.pathname;
-    
-        if (
-        pathname.startsWith("/manifest.webmanifest") ||
-        pathname.startsWith("/favicon") ||
-        pathname.startsWith("/apple-touch-icon") ||
-        pathname.startsWith("/_next") ||
-        pathname.startsWith("/assets") // if you have images/assets
-      ) {
-        return NextResponse.next();
-      }
-    
-      const isAuthPage = ["/login", "/signup", "/password-reset"].some(p => pathname.startsWith(p));
-      const isHome = pathname === "/";
-      const isUnrestrictedPage = ["/embed"].some(p => pathname.startsWith(p));
-      const isProtectedPage = !isAuthPage && !isUnrestrictedPage;
-    
-      if (!token) {
-        if (isProtectedPage) return NextResponse.redirect(new URL("/login", req.url));
-        return NextResponse.next();
-      }
-      
-      if(!pathname.startsWith("/lite/start") && !token.onboarding) {
+
+    if (
+      pathname.startsWith("/manifest.webmanifest") ||
+      pathname.startsWith("/favicon") ||
+      pathname.startsWith("/apple-touch-icon") ||
+      pathname.startsWith("/_next") ||
+      pathname.startsWith("/assets") // if you have images/assets
+    ) {
+      return NextResponse.next();
+    }
+
+    const isAuthPage = ["/login", "/signup", "/password-reset"].some(p => pathname.startsWith(p));
+    const isHome = pathname === "/";
+    const isUnrestrictedPage = ["/embed"].some(p => pathname.startsWith(p));
+    const isOnboardingPage = pathname.startsWith("/lite/start");
+    const isProtectedPage = !isAuthPage && !isUnrestrictedPage;
+
+    if (!token) {
+      if (isProtectedPage) return NextResponse.redirect(new URL("/login", req.url));
+      return NextResponse.next();
+    }
+
+    if (!isAuthPage && !isUnrestrictedPage) {
+      if (!token.onboarding && !isOnboardingPage) {
         return NextResponse.redirect(new URL("/lite/start", req.url));
       }
-    
-      if (isHome || isAuthPage ) {
+
+      if (token.onboarding && isOnboardingPage) {
         return NextResponse.redirect(new URL("/lite", req.url));
       }
+    }
+
+    if (isHome || isAuthPage) {
+      return NextResponse.redirect(new URL("/lite", req.url));
+    }
   }
 
   return NextResponse.next();
