aeafc773447865ce3cf83b6aa11ba7dfef0d1d85
aidev <dev@aiwebsiteservices.com>
2026-01-29 15:07:13 +0800

    added social links

--- src/app/(authenticated)/lite/(with-nav)/map/complete/feedback/page.tsx
+++ src/app/(authenticated)/lite/(with-nav)/map/complete/feedback/page.tsx
@@ -13,8 +13,8 @@ import { useState } from "react";
 export default function Page() {
   const [isSuccess, setIsSuccess] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const {currentUser} = useCurrentUserContext()
-  
+  const { currentUser } = useCurrentUserContext()
+
   // Form State
   const [rating, setRating] = useState(0);
   const [message, setMessage] = useState("");
@@ -22,8 +22,8 @@ export default function Page() {
 
   async function handleFormSubmit() {
     setErrorMsg("");
-    if(!currentUser) return
-    
+    if (!currentUser) return
+
     // Basic validation
     if (rating === 0) {
       setErrorMsg("Please select a star rating.");
@@ -60,7 +60,7 @@ export default function Page() {
         <div className="gap-4 text-center flex flex-col flex-nowrap justify-center h-full min-h-fit">
           <h1 className="header1 text-center mb-6">Questoria Feedback Form</h1>
           <p>Overall, how would you rate your adventure experience? </p>
-          
+
           <form
             onSubmit={(e) => {
               e.preventDefault();
@@ -78,11 +78,10 @@ export default function Page() {
                   className="transition-transform hover:scale-110 focus:outline-none"
                 >
                   <Star
-                    className={`h-[50px] stroke-black w-auto aspect-square transition-colors duration-200 ${
-                      starIndex <= rating
+                    className={`h-[50px] stroke-black w-auto aspect-square transition-colors duration-200 ${starIndex <= rating
                         ? "fill-yellow-400 text-yellow-400"
                         : "text-gray-400 fill-transparent"
-                    }`}
+                      }`}
                     size={24}
                   />
                 </button>
@@ -90,7 +89,7 @@ export default function Page() {
             </div>
 
             <p>Any feedback you’d like to share to the Questoria team?</p>
-            
+
             <textarea
               className="w-full bg-white input p-3 border rounded-md"
               name="feedback-message"
@@ -102,7 +101,7 @@ export default function Page() {
 
             {/* Error Message Display */}
             {errorMsg && (
-                <p className="text-red-500 font-bold">{errorMsg}</p>
+              <p className="text-red-500 font-bold">{errorMsg}</p>
             )}
 
             <button
@@ -136,13 +135,25 @@ function ThankYouPage() {
           />
           <p>Follow us on social media</p>
           <div className="flex flex-row flex-nowrap gap-3 items-center justify-center">
-            <div className="w-14 h-14 social linkedin"></div>
-            <div className="w-14 h-14 social fb"></div>
-            <div className="w-14 h-14 social insta"></div>
-            <div className="w-14 h-14 social tiktok"></div>
+            <a  href="https://www.linkedin.com/company/questoria-australia/" target="_blank">
+              <div className="w-14 h-14 social linkedin"></div>
+            </a>
+            <a className="cursor-pointer" href="https://www.facebook.com/profile.php?id=61584686996517" target="_blank">
+              <div className="w-14 h-14 social fb"></div>
+
+            </a>
+            <a className="cursor-pointer" href="https://www.instagram.com/questoriago" target="_blank">
+              <div className="w-14 h-14 social insta"></div>
+            </a>
+            <a className="cursor-pointer" href="https://www.tiktok.com/@questoriago" target="_blank"
+            >
+              <div className="w-14 h-14 social tiktok"></div>
+            </a>
+
+
           </div>
           <Link
-          href="/lite/map" className="!mt-10 w-full btn primary block py-3">
+            href="/lite/map" className="!mt-10 w-full btn primary block py-3">
             Return to Map
           </Link>
         </div>
